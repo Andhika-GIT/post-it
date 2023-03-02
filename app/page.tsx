a5d0e5434ae54894e87b7da6e0af181080c2de83
Andhika Prasetya <andhikapr081@gmail.com>
2023-03-02 21:41:06 +0700

    CreatePost.tsx setup the component for input and create post

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+'use client';
+import AddPost from './components/CreatePost';
+
 export default function Home() {
   return (
     <main>
       <h1>hello next</h1>
+      <AddPost />
     </main>
   );
 }
